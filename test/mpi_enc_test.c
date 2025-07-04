2d3c0df6afa80fdc0b9bcafdc1eba15daaf2e434
Chiel Prins <chiel@cpeb.it>
2025-07-04 21:21:02 +0200

    test

--- test/mpi_enc_test.c
+++ test/mpi_enc_test.c
@@ -1131,8 +1131,8 @@ void *enc_test(void *arg)
         goto MPP_TEST_OUT;
     }
 
-    if (cmd->kmpp_en)
-        kmpp_cfg_init(info);
+    //if (cmd->kmpp_en)
+    kmpp_cfg_init(info);
 
     ret = mpp_init(p->ctx, MPP_CTX_ENC, p->type);
     if (ret) {
